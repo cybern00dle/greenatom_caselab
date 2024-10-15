78fe71e8ccc4508d195425a0711ef66f9da28b74
cybern00dle <koshech_noskich@mail.ru>
2024-10-15 14:33:40 +0300

    first commit

---
+++ reviewapp/models.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class Review(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    sentiment = models.CharField(max_length=8, blank=True, null=True)
+    score = models.PositiveSmallIntegerField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
